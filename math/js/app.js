44550567c91b0890b8178043c409889b2ce5502f
NikitaMusikhin <nikita-musikhin@yandex.ru>
2015-11-09 23:46:37 +0500

    sec

--- math/js/app.js
+++ math/js/app.js
@@ -1,11 +1,21 @@
 'use strict';
-angular.module('MathApp', ['ui.router','Ctrls']).
-config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
-$urlRouterProvider.otherwise("/reg");
-$stateProvider.
-  state('reg', {
-    url:'/reg',
-    templateUrl: 'application/views/reg_view.html',
-    controller: 'regCtrl'
-  });
+angular.module('MathApp', ['ui.router','User'])
+.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+$urlRouterProvider.otherwise("/login");
+$stateProvider
+    .state('reg', {
+        url:'/reg',
+        templateUrl: '/application/views/reg_view.html',
+        controller: 'regCtrl'
+    })
+    .state('login', {
+        url:'/login',
+        templateUrl: '/application/views/login_view.html',
+        controller: 'loginCtrl'
+    })
+    .state('dashboard',{
+        url:'/dashboard',
+        templateUrl: '/application/views/dashboard_view.html',
+        controller: 'profCtrl'
+    });
 }]);
