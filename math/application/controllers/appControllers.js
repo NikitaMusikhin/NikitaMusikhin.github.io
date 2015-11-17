7d7e8bcabc616c7b019552444a015fc3f25a0c8e
NikitaMusikhin <nikita-musikhin@yandex.ru>
2015-11-17 18:06:01 +0500

    edit

--- math/application/controllers/appControllers.js
+++ math/application/controllers/appControllers.js
@@ -1,1 +1,258 @@
-angular.module("User",["ngResource","ngStorage"]).controller("regCtrl",["$scope","$location","UserService",function(e,t,r){e.addNewUser=function(n){r.user.create(n,function(e){alert("Поздравляем,вы успешно зарегистрированы!"),t.path("/login")},function(){alert("So sad")}),e.regForm.$setPristine(),e.user={}},e.getError=function(e){if(angular.isDefined(e)){if(e.required)return"Поле не должно быть пустым";if(e.minlength)return"В данное поле должно быть введено более 6 символов";if(e.email)return"Введите корректные данные"}}}]).controller("loginCtrl",["$scope","$rootScope","$http","$location","UserService","TokenService",function(e,t,r,n,o,a){e.authorize=function(e){o.auth.login(e,function(e){a.setToken(e.token),t.user=e.user,n.path("/dashboard")},function(){alert(":(")})}}]).controller("profCtrl",["$scope","$rootScope","$localStorage","$location","UserService",function(e,t,r,n,o){function a(e){var t=e;this.__defineGetter__("qty",function(){return t}),this.__defineSetter__("qty",function(e){t=parseInt(e)})}e.logout=function(){o.out.logout({},function(e){delete r.token,n.path("/login")},function(){alert(":(")})},r.token?t.user||o.mathContext.isToken({},function(e){t.user=e.user},function(){n.path("/login")}):n.path("/login"),t.$watch("user",function(){e.profile=t.user}),e.qty_add=new a(2),e.qty_sub=new a(2),e.qty_mull=new a(2),e.qty_div=new a(2),e.selectoObj=function(t){switch(t){case"add":return e.qty_add.qty;case"sub":return e.qty_sub.qty;case"mull":return e.qty_mull.qty;case"div":return e.qty_div.qty}},e.setExampleSettings=function(t){r.typeExample=t,r.modeExample=e.selectoObj(t)}}]).controller("exampleCtrl",["$scope","$rootScope","$localStorage","$location","UserService","TokenService",function(e,t,r,n,o,a){r.token?t.user||o.mathContext.isToken({},function(e){t.user=e.user},function(){n.path("/login")}):n.path("/login"),t.$watch("user",function(){e.profile=t.user}),r.typeExample&&r.modeExample?e.settings={type:r.typeExample,mode:r.modeExample}:n.path("/dashboard"),e.counter={wrong:0,right:0,overall:0},e.view={blockInput:!0,blockAnswer:!1},e.startSolutions=function(){e.counter.overall=0,e.counter.right=0,e.counter.wrong=0,e.view.blockInput=!1,e.view.blockAnswer=!0,e.generateNewExample()},e.generateNewExample=function(){if(e.counter.overall<10){var t=e.getNumbers();switch(e.settings.type){case"add":var r=t.reduce(function(e,t){return e+t});e.example={primer:t.join(" + ")+" = "+r,otvet:parseInt(r)};break;case"sub":var r=t.reduce(function(e,t){return e-t});e.example={primer:t.join(" - ")+" = "+r,otvet:parseInt(r)};break;case"mull":var r=t.reduce(function(e,t){return e*t});e.example={primer:t.join(" * ")+" = "+r,otvet:parseInt(r)};break;case"div":var r=t.reduce(function(e,t){return e/t});e.example={primer:t.join(" / ")+" = "+r,otvet:parseInt(r)};break;default:n.path("/dashboard")}e.exampleReady=!0,e.example.userAnswer=parseInt("")}else e.sendResult(),e.example.userAnswer="",e.view.blockInput=!0,e.view.blockAnswer=!1},e.sendResult=function(){switch(e.settings.type){case"add":alert(JSON.stringify(e.counter));break;case"sub":alert(JSON.stringify(e.counter));break;case"mull":alert(JSON.stringify(e.counter));break;case"div":alert(JSON.stringify(e.counter));break;default:alert("error")}},e.saveSolution=function(t){t==e.example.otvet?e.counter.right++:e.counter.wrong++,e.counter.overall++},e.getNumbers=function(){for(var t=[],r=0;r<e.settings.mode;r++)t.push(e.getRandomNumber());return t},e.getRandomNumber=function(){return Math.floor(6*Math.random()+1)}}]);
+angular.module('User',['ngResource','ngStorage'])
+.controller('regCtrl',['$scope','$location','UserService',
+function($scope,$location,UserService){
+    $scope.addNewUser = function(user){
+        UserService.user.create(user,function success(newUser){
+            alert('Поздравляем,вы успешно зарегистрированы!');
+            $location.path('/login');
+        },function err(){
+            alert('So sad');
+        });
+        $scope.regForm.$setPristine();
+        $scope.user = {};
+    }
+    $scope.getError = function(err){
+        if(angular.isDefined(err)){
+            if(err.required){
+                return "Поле не должно быть пустым";
+            }
+            if(err.minlength){
+                return "В данное поле должно быть введено более 6 символов";
+            }
+            if(err.email){
+                return "Введите корректные данные";
+            }
+        }
+    }
+}])
+.controller('loginCtrl',['$localStorage','$scope','$rootScope','$http','$location','UserService',
+function($localStorage,$scope,$rootScope,$http,$location,UserService){
+    $scope.authorize = function(user){
+        UserService.auth.login(user,function success(data){
+             $localStorage.token = data.token;
+             $rootScope.user = data.user;
+             $location.path('/dashboard');
+        },function err(){
+            alert(':(');
+        });
+    };
+}])
+.controller('profCtrl',['$http','$scope','$rootScope','$localStorage','$location','UserService',
+function($http,$scope,$rootScope,$localStorage,$location,UserService){
+    $scope.logout = function() {
+        $http.defaults.headers.common['token'] = $localStorage.token;
+        UserService.logout.delete({},
+        function success(data){
+            delete $localStorage.token;
+            delete $localStorage.typeExample;
+            delete $localStorage.modeExample;
+            $location.path('/login');
+        },function err(){
+            alert(':(');
+        });
+    };
+    if($localStorage.token){
+        if(!$rootScope.user){
+            UserService.mathContext.isToken({},function success(data){
+                $rootScope.user = data.user;
+            },function err(){
+                $location.path('/login');
+            });
+        }
+    }else{
+        $location.path('/login');
+    };
+    $rootScope.$watch('user',function() {
+        $scope.profile = $rootScope.user;
+    });
+    $scope.rangeSettings = {
+        add:2,
+        sub:2,
+        mull:2,
+        div:2
+    }
+    $scope.selectoObj = function(operation){
+        switch (operation) {
+            case 'add':
+                return $scope.rangeSettings.add;
+                break;
+            case 'sub':
+                return $scope.rangeSettings.sub;
+                break;
+            case 'mull':
+                return $scope.rangeSettings.mull;
+                break;
+            case 'div':
+                return $scope.rangeSettings.div;
+                break;
+        }
+    }
+
+    $scope.setExampleSettings = function(operation){
+        $localStorage.typeExample = operation;
+        $localStorage.modeExample = $scope.selectoObj(operation);
+    };
+}])
+.controller('exampleCtrl',['$http','$scope','$rootScope','$localStorage','$location','UserService',
+function($http,$scope,$rootScope,$localStorage,$location,UserService){
+    if($localStorage.token){
+        if(!$rootScope.user){
+            UserService.mathContext.isToken({},function success(data){
+                $rootScope.user = data.user;
+            },function err(){
+                $location.path('/login');
+            });
+        }
+        $http.defaults.headers.common['token'] = $localStorage.token;
+        UserService.range.get({},function success(data){
+            $scope.range = $scope.returnTypeRange(data);
+        },function err(){
+            alert('error localstorage token');
+        });
+    }else{
+        $location.path('/login');
+    }
+
+    $scope.returnTypeRange = function(obj){
+        switch ($scope.settings.type) {
+            case 'add':
+                return obj.addition;
+                break;
+            case 'sub':
+                return obj.substraction;
+                break;
+            case 'mull':
+                return obj.multiplication;
+                break;
+            case 'div':
+                return obj.division;
+                break;
+            }
+    }
+
+    $rootScope.$watch('user',function() {
+        $scope.profile = $rootScope.user;
+    });
+//установка режима решения и типа задач
+    if($localStorage.typeExample && $localStorage.modeExample){
+        $scope.settings = {
+            type:$localStorage.typeExample,
+            mode:$localStorage.modeExample
+        }
+    }else{
+        $location.path('/dashboard');
+    }
+//логика работы генератора
+    $scope.counter = {
+        wrong:0,
+        right:0,
+        overall:0
+    }
+
+    $scope.view = {
+        blockInput:true,
+        blockAnswer:false
+    }
+
+    $scope.example = {
+        otvet  : 0,
+        primer : '',
+        type   : ''
+    }
+
+    $scope.startSolutions = function(){
+        $scope.counter.overall = 0;
+        $scope.counter.right = 0;
+        $scope.counter.wrong = 0;
+        $scope.view.blockInput = false;
+        $scope.view.blockAnswer = true;
+        $scope.generateNewExample();
+    }
+
+    $scope.generateNewExample = function(){
+        if($scope.counter.overall < 10){
+            var numbers = $scope.getNumbers();
+            switch ($scope.settings.type) {
+                case 'add':
+                    $scope.example.type = 'ADDITION_BASIC';
+                    $scope.example.otvet = numbers.reduce(function(x,y){
+                        return x + y;
+                    });
+                    $scope.example.primer = numbers.join(' + ')+' = '+ $scope.example.otvet;
+                    break;
+                case 'sub':
+                    $scope.example.type = 'SUBTRACTION_BASIC';
+                    $scope.example.otvet = numbers.reduce(function(x,y){
+                        return x - y;
+                    });
+                    $scope.example.primer = numbers.join(' - ')+' = '+ $scope.example.otvet;
+                    break;
+                case 'mull':
+                    $scope.example.type = 'MULTIPLICATION_BASIC';
+                    $scope.example.otvet = numbers.reduce(function(x,y){
+                        return x * y;
+                    });
+                    $scope.example.primer = numbers.join(' * ')+' = '+ $scope.example.otvet;
+                    break;
+                case 'div':
+                    $scope.example.type = 'DIVISION_BASIC';
+                    $scope.example.otvet = numbers.reduce(function(x,y){
+                        return x / y;
+                    });
+                    $scope.example.primer = numbers.join(' / ')+' = '+ $scope.example.otvet;
+                    break;
+                default:
+                    $location.path('/dashboard');
+            }
+            $scope.exampleReady = true;
+        }else{
+            $scope.sendResult();
+            $scope.example.userAnswer = '';
+            $scope.view.blockInput = true;
+            $scope.view.blockAnswer = false;
+            //delete $localStorage.typeExample;
+            //delete $localStorage.modeExample;
+        }
+    }
+
+    $scope.sendResult = function(){
+        $http.defaults.headers.common['token'] = $localStorage.token;
+        UserService.exampleAPI.save({
+            typeExample:$scope.example.type,
+            right:$scope.counter.right,
+            wrong:$scope.counter.wrong,
+            count:$localStorage.modeExample
+        },
+        function success(){
+            alert('success');
+            /*$http.defaults.headers.common['token'] = $localStorage.token;//сразу обновляем количество правильно решенных
+
+            UserService.range.get({},function success(data){
+                $scope.range = $scope.returnTypeRange(data);
+            },function err(){
+                alert('error localstorage token');
+            });*/
+        },function err(){
+            alert('error');
+        });
+    }
+
+    $scope.saveSolution = function(otvet){
+        otvet == $scope.example.otvet?$scope.counter.right++:$scope.counter.wrong++;
+        $scope.counter.overall++;
+        $scope.example.userAnswer = '';
+    }
+
+    $scope.getNumbers = function(){
+        var numbers = [];
+        for(var i = 0;i < $scope.settings.mode;i++){
+            numbers.push($scope.getRandomNumber());
+        }
+        return numbers;
+    }
+
+    $scope.getRandomNumber = function(){
+        //эта функция хавает число верных ответов и генерит числа в зависимости от соотношения диапазона и количества верных ответов этого
+        return Math.floor((Math.random()*6)+1);
+    }
+}]);
--- math/application/services/user.js
+++ math/application/services/user.js
@@ -1,1 +1,33 @@
-angular.module("User").constant("urlForResource","http://178.47.139.131\\:9000/").factory("TokenService",["$localStorage",function(e){$storage=e;var t="";return{setToken:function(e){$storage.token=e},getToken:function(){return t}}}]).factory("UserService",["$http","$resource","$localStorage","urlForResource","TokenService",function(e,t,o,r,n){return{auth:t(r+"authorization/user",{},{login:{method:"POST"}}),out:t(r+"logout/user",{},{logout:{method:"DELETE",headers:{token:n.getToken()}}}),user:t(r+"registration/user",{},{create:{method:"POST"}}),health:t(r+"health",{},{isHealth:{method:"GET"}}),mathContext:t(r+"context/mathematics",{},{isToken:{method:"GET",headers:{token:o.token}}}),rangeExample:t(r+"context/examples",{},{range:{method:"GET",headers:{token:o.token}}}),addition:t(r+"example/addition/basic",{},{solutions:{method:"POST",headers:{token:o.token}}})}}]);
+angular.module('User')
+.constant('urlForResource','http://178.47.139.131\\:9000/')
+.factory('UserService',['$http','$resource','$localStorage','urlForResource',
+function($http,$resource,$localStorage,urlForResource){
+    return {
+        auth : $resource(urlForResource+'authorization/user',{},{
+            login:{
+                method:'POST'
+            }
+        }),
+        user : $resource(urlForResource+'registration/user',{},{
+            create:{
+                method:'POST'
+            }
+        }),
+        health : $resource(urlForResource+'health',{},{
+            isHealth:{
+                method:'GET'
+            }
+        }),
+        mathContext : $resource(urlForResource+'context/mathematics',{},{
+            isToken:{
+                method:'GET',
+                headers:{
+                   'token':$localStorage.token
+                }
+            }
+        }),
+        logout: $resource(urlForResource+'logout/user'),
+        range : $resource(urlForResource+'context/examples'),
+        exampleAPI : $resource(urlForResource+'example')
+    }
+}]);
